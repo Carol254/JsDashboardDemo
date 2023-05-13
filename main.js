9eba6321d97640a6f39f2018c7fdbc02ec85ad68
Caroline <wanguic49@gmail.com>
2023-05-14 00:13:22 +0300

    completed the first chart

--- main.js
+++ main.js
@@ -14,3 +14,49 @@ function closeSideBar(){
         sidebarOpen = false;
     }
 }
+
+// BAR CHART
+var barChartOptions = {
+    chart: {
+      type: 'bar',
+      height:350,
+      toolbar:{
+        show:false
+      },
+    },
+    colors:[
+        "#246dec",
+        "#cc3c43",
+        "#367952",
+        "#f5b741",
+        "#4f35a1"
+    ],
+    plotOptions: {
+      bar: {
+        distributed:true,
+        borderRadius:4,
+        horizontal:false,
+        columnWidth:'40%',
+      }
+    },
+    series: [{
+      data: [3,2,4,5,6]
+    }],
+    dataLabels:{
+        enabled:false
+    },
+    legend:{
+        show:false
+    },
+    xaxis:{
+        categories:['Monitor','Laptop','HeadPhones','Earphones','Airpods']
+    },
+    yaxis:{
+        title:{
+            text:'Count'
+        }
+    }
+  };
+
+  var barChart = new ApexCharts(document.querySelector("#bar-chart"),barChartOptions);
+  barChart.render();
