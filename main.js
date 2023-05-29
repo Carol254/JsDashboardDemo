9967c02c09b79049927ab0f9dc172b9b9cef8a9d
Caroline <wanguic49@gmail.com>
2023-05-29 16:37:05 +0300

    few changes made

--- main.js
+++ main.js
@@ -111,7 +111,65 @@ var barChartOptions = {
   var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
   areaChart.render();
   
-  $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
-  });
   
+  
+ 
+
+  let routes = {};
+  let templates = {};
+
+
+let app_div = document.getElementById('app');
+
+  function productsView(){
+    let div = document.createElement('div');
+    let link = document.createElement('a');
+
+    link.href = '#/products';
+    link.innerText = 'Products';
+
+    div.innerHTML = '<h1>Products</h1>';
+    div.appendChild(link);
+
+    app_div.appendChild(div);
+
+  };
+
+  function route (path, template) {
+    if (typeof template === 'function') {
+        return routes[path] = template;
+    }
+    else if (typeof template === 'string') {
+        return routes[path] = templates[template];
+    } else {
+        return;
+    };
+};
+
+function template (name, templateFunction) {
+  return templates[name] = templateFunction;
+};
+
+template('products', function(){
+  productsView();
+});
+
+route('/', 'products');
+
+function resolveRoute(route) {
+  try {
+      return routes[route];
+  } catch (e) {
+      throw new Error(`Route ${route} not found`);
+  };
+};
+
+function router(evt) {
+  let url = window.location.hash.slice(1) || '/';
+  let route = resolveRoute(url);
+
+  route();
+};
+
+window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
